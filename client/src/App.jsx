479f9acadc3b64405c33205479440977f4b9b64d
myarask <myaraskavitch@gobonfire.com>
2020-03-14 10:59:20 -0400

    convert to functional component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,87 +1,59 @@
 // src/App.js
 
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import { getInitialFlightData } from "./DataProvider";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: getInitialFlightData()
-    };
-
-    this.eventSource = new EventSource("http://localhost:5000/events");
-
-    this.columns = [
-      {
-        Header: "Origin",
-        accessor: "origin"
-      },
-      {
-        Header: "Flight",
-        accessor: "flight"
-      },
-      {
-        Header: "Arrival",
-        accessor: "arrival"
-      },
-      {
-        Header: "State",
-        accessor: "state"
-      }
-    ];
+const columns = [
+  {
+    Header: "Origin",
+    accessor: "origin"
+  },
+  {
+    Header: "Flight",
+    accessor: "flight"
+  },
+  {
+    Header: "Arrival",
+    accessor: "arrival"
+  },
+  {
+    Header: "State",
+    accessor: "state"
   }
+];
 
-  componentDidMount() {
-    this.eventSource.addEventListener("flightStateUpdate", e =>
-      this.updateFlightState(e)
-    );
-    this.eventSource.addEventListener("flightRemoval", e =>
-      this.removeFlight(e)
-    );
-    this.eventSource.addEventListener("closedConnection", e =>
-      this.stopUpdates()
-    );
-  }
+const App = () => {
+  const [data, setData] = useState(getInitialFlightData());
 
-  updateFlightState(e) {
-    const flightState = JSON.parse(e.data);
+  useEffect(() => {
+    const eventSource = new EventSource("http://localhost:5000/events");
 
-    let newData = this.state.data.map(item => {
-      if (item.flight === flightState.flight) {
-        item.state = flightState.state;
-      }
-      return item;
+    eventSource.addEventListener("flightStateUpdate", e => {
+      const { state, flight } = JSON.parse(e.data);
+
+      setData(prev =>
+        prev.map(x => ({
+          ...x,
+          state: x.flight === flight ? state : x.state
+        }))
+      );
     });
 
-    this.setState(Object.assign({}, { data: newData }));
-  }
+    eventSource.addEventListener("flightRemoval", e => {
+      const { flight } = JSON.parse(e.data);
 
-  removeFlight(e) {
-    const flightInfo = JSON.parse(e.data);
+      setData(prev => prev.filter(x => x.flight !== flight));
+    });
 
-    const newData = this.state.data.filter(
-      item => item.flight !== flightInfo.flight
-    );
+    eventSource.addEventListener("closedConnection", () => {
+      eventSource.close();
+      alert("closed");
+    });
+  }, []);
 
-    this.setState(Object.assign({}, { data: newData }));
-  }
-
-  stopUpdates() {
-    this.eventSource.close();
-    alert("closed");
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={() => this.stopUpdates()}>Stop updates</button>
-        <ReactTable data={this.state.data} columns={this.columns} />
-      </div>
-    );
-  }
-}
+  return <ReactTable data={data} columns={columns} />;
+};
 
 export default App;
